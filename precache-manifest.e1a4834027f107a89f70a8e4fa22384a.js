fd083f0ad019e30ac9d2a4384cd95e39abfe59bc
rajika111 <poower@gmail.com>
2019-11-06 17:00:29 +0300

    Updates

---
+++ precache-manifest.e1a4834027f107a89f70a8e4fa22384a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059948563b7a74f31c83e19c4eeb522f",
+    "url": "/Crypto-price/index.html"
+  },
+  {
+    "revision": "cd7965cd38e9b52c9f2b",
+    "url": "/Crypto-price/static/css/main.80e4681a.chunk.css"
+  },
+  {
+    "revision": "fad0d66e9411bc10ffbc",
+    "url": "/Crypto-price/static/js/2.39b4b49c.chunk.js"
+  },
+  {
+    "revision": "cd7965cd38e9b52c9f2b",
+    "url": "/Crypto-price/static/js/main.2dc7b826.chunk.js"
+  },
+  {
+    "revision": "52b91fcd368221728bf9",
+    "url": "/Crypto-price/static/js/runtime-main.9520cc9f.js"
+  }
+]);
